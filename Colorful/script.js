dd0fd13966d7361c645d44da29bd25648ee56118
Diramix <79011730+Diramix@users.noreply.github.com>
2024-09-18 23:31:41 +0300

    fix change theme script

--- Colorful/script.js
+++ Colorful/script.js
@@ -1,7 +1,9 @@
 // Скрипт для смены темы
 setInterval(() => {
-  const changeTheme = document.querySelector('.ym-light-theme');
-  if (changeTheme) {
-    changeTheme.classList.replace('ym-light-theme', 'ym-dark-theme');
+  const body = document.body;
+  if (!body.classList.contains('ym-dark-theme') && !body.classList.contains('ym-light-theme')) {
+    body.classList.add('ym-dark-theme');
+  } else if (body.classList.contains('ym-light-theme')) {
+    body.classList.replace('ym-light-theme', 'ym-dark-theme');
   }
 }, 0);
