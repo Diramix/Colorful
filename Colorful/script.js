eb06b672aac2c0db0cbfea93b31f6cf6e479fd8e
Diramix <danyagranny10@gmail.com>
2024-11-06 00:23:23 +0300

    Colorful II

--- Colorful/script.js
+++
@@ -1,9 +0,0 @@
-// Скрипт для смены темы
-setInterval(() => {
-  const body = document.body;
-  if (!body.classList.contains('ym-dark-theme') && !body.classList.contains('ym-light-theme')) {
-    body.classList.add('ym-dark-theme');
-  } else if (body.classList.contains('ym-light-theme')) {
-    body.classList.replace('ym-light-theme', 'ym-dark-theme');
-  }
-}, 0);
