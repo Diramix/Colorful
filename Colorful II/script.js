2b0d0c1c7793574f90c82e62dba73e795c29dbdb
Diramix <79011730+Diramix@users.noreply.github.com>
2024-11-21 20:26:47 +0300

    add theme changer script & fixes

---
+++ Colorful II/script.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+  const body = document.body;
+  if (!body.classList.contains('ym-dark-theme') && !body.classList.contains('ym-light-theme')) {
+    body.classList.add('ym-dark-theme');
+  } else if (body.classList.contains('ym-light-theme')) {
+    body.classList.replace('ym-light-theme', 'ym-dark-theme');
+  }
+}, 0);
