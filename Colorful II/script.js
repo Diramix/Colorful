4d2895c84bff073e219b0534f05b69f745d95c75
Diramix <danyagranny10@proton.me>
2025-03-03 21:45:37 +0300

    Migration to PulseSync 2.6.0

--- Colorful II/script.js
+++ Colorful II/script.js
@@ -1,11 +1,9 @@
 /*Авто смена темы Яндекс Музыки на тёмную*/
 /*--------------------------------------------*/
-setInterval(() => {
-  const body = document.body;
-  if (!body.classList.contains('ym-dark-theme') && !body.classList.contains('ym-light-theme')) {
-    body.classList.add('ym-dark-theme');
-  } else if (body.classList.contains('ym-light-theme')) {
-    body.classList.replace('ym-light-theme', 'ym-dark-theme');
-  }
-}, 0);
+const body = document.body;
+if (!body.classList.contains('ym-dark-theme') && !body.classList.contains('ym-light-theme')) {
+	body.classList.add('ym-dark-theme');
+} else if (body.classList.contains('ym-light-theme')) {
+	body.classList.replace('ym-light-theme', 'ym-dark-theme');
+}
 /*--------------------------------------------*/
